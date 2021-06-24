3d01d70dbb23d2f69674421f8550869d45609bc6
thisvalera <valleerroonn4ikSho@gmail.com>
2021-06-24 19:04:38 +0300

    register-finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,4 @@
 $(function () {
-  $(".menu__item").on("click", function () {
-    $(".menu__item").removeClass("menu__item--active");
-    $(this).toggleClass("menu__item--active");
-  });
   $(".submenu__link").on("click", function () {
     $(".submenu__link").removeClass("submenu__link--active");
     $(this).toggleClass("submenu__link--active");
@@ -15,4 +11,5 @@ $(function () {
     arrows: true,
     dots: false,
 });
+
 });
