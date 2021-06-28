e19e7672a6908ccef610ab2b2d8efc92ace3207f
macenuw <gnykabiny54@gmail.com>
2021-06-28 18:46:02 +0300

    more work

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
-  $(".submenu__link").on("click", function () {
-    $(".submenu__link").removeClass("submenu__link--active");
-    $(this).toggleClass("submenu__link--active");
+  $(".menu__item").on("click", function () {
+    $(".menu__item").removeClass("menu__item--active");
+    $(this).toggleClass("menu__item--active");
   });
   $('.home__select').styler();
 
@@ -10,6 +10,43 @@ $(function () {
     slidesToScroll: 1,
     arrows: true,
     dots: false,
-});
+  });
 
 });
+
+
+
+function addedCheckPr() {
+  var element = document.getElementById("pr-span");
+  element.classList.toggle("active");
+}
+
+function addedCheckCto() {
+  var element = document.getElementById("cto-span");
+  element.classList.toggle("active");
+}
+
+
+
+
+// modal open
+function openModalAuthMyOrder() {
+  var modalAuthMyOrder = document.getElementById("modalAuthMyOrder");
+  modalAuthMyOrder.classList.toggle("active")
+}
+function openModalAuthMyInfo() {
+  var modalAuthMyInfo = document.getElementById("modalAuthMyInfo");
+  modalAuthMyInfo.classList.toggle("active")
+}
+function openModalAuthMyCto() {
+  var modalAuthMyCto = document.getElementById("modalAuthMyCto");
+  modalAuthMyCto.classList.toggle("active")
+}
+function openModalAuthMyPark() {
+  var modalAuthMyPark = document.getElementById("modalAuthMyPark");
+  modalAuthMyPark.classList.toggle("active")
+}
+function openModalChoiceYou() {
+  var modalChoiceYou = document.getElementById("choiceWhoAreYou");
+  modalChoiceYou.classList.toggle("active")
+}
